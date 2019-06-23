adbcfad47a71d276b3c092e2bff3971a8b792bb1
Dipesh Bhatevera <35795916+DEEPESH98@users.noreply.github.com>
2019-06-23 17:34:10 +0530

    Update students_pie_chart.py

--- students_pie_chart.py
+++ students_pie_chart.py
@@ -1,8 +1,22 @@
+'''
+student.csv
+1,Dipesh,18,123456,12,70
+2,Ayush,18,8765314,13,80
+3,Jeenal,19,546465,11,70
+4,Harshita,20,897954,16,70
+5,Hinal,19,32654,17,95
+'''
+
+
+
+
+
 # make a students marks table pie chart
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import time
+import time,student_name,age,contact,study_hour,marks
+
 
 #reading data from a cvs file
 df=pd.read_csv('student.csv')
